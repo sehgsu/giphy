637e590a4fa871ffc8999ece24f9df152b753bc6
Alex <alex@Alex.local>
2019-04-05 23:25:59 -0400

    nearly fini

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # giphy
 
 PSUEDO
-set up html div and buttons
-set up variables for buttons, images, and ajax calls
-generate buttons
-on click events for buttons to generate gifs
-on click for gifs to play
+√ set up html div and buttons
+√ set up variables for buttons,
+√ ajax calls
+√ generate buttons
+√ on click events for buttons to generate gifs
+√ on click for gifs to play
 
-dev notes: 
+dev notes: next is getting the display to stay as a Still and the on click to animate them
---
+++ assets/app.js
@@ -0,0 +1,54 @@
+var characters = ["Tyrion Lannister", "Lord Varys", "Bran Stark", "Thoros", "The Hound"];
+
+function genButtons(){
+    $("#btn-view").empty();
+    for (var i = 0; i < characters.length; i++) {
+        var b = $("<button>");
+        b.addClass("char-btn btn-dark");
+        b.attr("data-name", characters[i]);
+        b.text(characters[i]);
+        b.appendTo("#btn-view");
+    }
+    console.log(characters);
+}
+$("#new-button").on("click", function (event) {
+    event.preventDefault();
+    var newBtn = $("#char-search").val().trim();
+    characters.push(newBtn);
+    genButtons();
+    
+})
+
+
+$(document).on("click", ".char-btn", function () {
+    var character = $(this).attr("data-name");
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=A37MnA26KQDjAUmr1dOT3AEcnBEPBqOR&q=" + character + "&limit=5";
+    
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            gifDiv.addClass("newGifs");
+            var rate = $("<p>").text("Rated: " + results[i].rating);
+            gifDiv.append(rate);
+            var charImg = $("<img>");
+            charImg.addClass("gif");
+            charImg.attr("alt", "character image");
+            charImg.attr("src", results[i].images.fixed_height.url);
+            charImg.attr("data-still", results[i].images.fixed_height_still.url);
+            charImg.attr("data-animate", results[i].images.fixed_height.url);
+            charImg.attr("data-state", "still");
+            gifDiv.append(charImg);
+            $("#gifs").prepend(gifDiv);
+
+        }
+        
+    
+    })
+})
+
+genButtons();
